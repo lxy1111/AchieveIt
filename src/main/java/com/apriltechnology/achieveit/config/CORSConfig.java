ad069bf424ba4161c07b7e226faa8cd32c79c7ef
Wu Wenhan <1020645987@qq.com>
2020-03-24 01:23:58 +0800

    Add @Configuration Tag

--- src/main/java/com/apriltechnology/achieveit/config/CORSConfig.java
+++ src/main/java/com/apriltechnology/achieveit/config/CORSConfig.java
@@ -1,10 +1,11 @@
 package com.apriltechnology.achieveit.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+@Configuration
 public class CORSConfig {
     @Bean
     public CorsFilter corsFilter() {
