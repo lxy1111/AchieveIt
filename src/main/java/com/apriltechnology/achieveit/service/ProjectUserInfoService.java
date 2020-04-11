ed7e918b38365feb8a2c64760d9d43a33409648f
bran <821655671@qq.com>
2020-04-11 10:39:05 +0800

    修改新增成员bug

--- src/main/java/com/apriltechnology/achieveit/service/ProjectUserInfoService.java
+++ src/main/java/com/apriltechnology/achieveit/service/ProjectUserInfoService.java
@@ -29,6 +29,7 @@ public interface ProjectUserInfoService {
      */
     Pair<Boolean,String> projectUserDelete(Long projectId,Long userId);
 
+    Pair<Boolean,String> judgeProjectUserInfo(ProjectUserAdd projectUserAdd);
 
     /**
      * 添加组员信息
